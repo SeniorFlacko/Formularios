c6f9504076b6a41ba264fffd4e063541c52fd943
seniorflacko <cristianalemanfuentes@gmail.com>
2017-08-26 19:49:37 -0500

    FormGroup anidado y basado en un objeto modelo

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,14 +10,30 @@ export class DataComponent implements OnInit {
 
   forma:FormGroup;
 
+  usuario:Object = {
+    
+    nombreCompleto: {
+      nombre:"Cristian",
+      apellido:"Aleman"
+    },
+    correo:"joker_psy@hotmail.es"
+
+  }
+
+
   constructor() { 
     this.forma =  new FormGroup({
-      'nombre': new FormControl('',[
+
+      nombrecompleto: new FormGroup({
+        'nombre': new FormControl('',[
                                       Validators.required,
                                       Validators.minLength(3)
                                    ]
                                ),
-      'apellido': new FormControl('',Validators.required),
+        'apellido': new FormControl('',Validators.required),
+      }),
+
+      
       'correo': new FormControl('',[
                                       Validators.required,
                                       Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
